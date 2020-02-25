0df3bd9b6b7146c04654206d58de7c812cef9e2a
RobinLinus <RobinLinus@users.noreply.github.com>
2020-02-25 16:04:23 +0100

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,15 @@
+location = encodeURIComponent(`data:text/html, 
+<head>
+    <meta charset="utf-8">
+    <title>Secure Bookmark Demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" type="image/x-icon" href="https://https://bitcoin.robinlinus.com/images/icon-192x192.png">
+    <meta name="description" content="Secure Bookmark Demo">
+    <!-- Homescreen icons -->
+    <link rel="apple-touch-icon" sizes="144x144" href="https://https://bitcoin.robinlinus.com/images/icon-144x144.png">
+    <meta name="apple-mobile-web-app-capable" content="no">
+    <link rel="manifest" href="manifest.json">
+</head>
+<h1>Secure Bookmark Demo</h1>
+Bookmark this page to test the favicon.
+`)
