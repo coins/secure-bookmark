abf2a5babb17b3a78a13811176879867720c8036
RobinLinus <RobinLinus@users.noreply.github.com>
2020-02-25 16:14:21 +0100

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -3,10 +3,10 @@ location = 'data:text/html,'+encodeURIComponent(`
     <meta charset="utf-8">
     <title>Secure Bookmark Demo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="shortcut icon" type="image/x-icon" href="https://https://bitcoin.robinlinus.com/images/icon-192x192.png">
+    <link rel="shortcut icon" type="image/x-icon" href="https://bitcoin.robinlinus.com/images/icon-192x192.png">
     <meta name="description" content="Secure Bookmark Demo">
     <!-- Homescreen icons -->
-    <link rel="apple-touch-icon" sizes="144x144" href="https://https://bitcoin.robinlinus.com/images/icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="https://bitcoin.robinlinus.com/images/icon-144x144.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
 </head>
 <h1>Secure Bookmark Demo</h1>
