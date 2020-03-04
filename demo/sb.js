6f95ad7fcedae05c33d725d67334726316b7d3ab
RobinLinus <robinlinus@users.noreply.github.com>
2020-03-04 17:55:39 +0100

    Simplify bitcoin demo app

--- demo/sb.js
+++ demo/sb.js
@@ -6,9 +6,16 @@
 /**
  * 1: Create some random seed
  */
-let seed = new Uint8Array(32)
-crypto.getRandomValues(seed)
-seed = Array.from(seed)
+let seed;
+let urlFragment = location.hash.substr(1)
+if (urlFragment) {
+    seed = JSON.parse(urlFragment)
+} else {
+    seed = new Uint8Array(32)
+    crypto.getRandomValues(seed)
+    seed = Array.from(seed)
+    history.pushState(0, 0, location.href + '#' + JSON.stringify(seed))
+}
 
 /**
  * 2: Bootstrap the app html and store the seed by hard-coding it
@@ -47,6 +54,7 @@ const bodyHTML = `<html>
     <h1>Bitcoin Demo App</h1>
     <h2>DISCLAIMER: EXPERIMENTAL DEMO</h2>
     DON'T USE REAL MONEY !! THIS IS EXPERIMENTAL CODE. 
+    YOUR SECRET KEY IS EXPOSED IN YOUR BROWSER'S HISTORY
     <h2>Your Address</h2>
     <div>
       <a id="el_address" target="_blank"></a>
@@ -63,7 +71,6 @@ const bodyHTML = `<html>
     <script src="https://coins.github.io/secure-bookmark/demo/clipboard.js" integrity="sha256-3VCByRM+Ge37Dm+yksb03tsaCAq9n1rDui/BpHhyIMA=" crossorigin></script>
 
     <script>
-      const seed = ${JSON.stringify(seed)}
       const address = Bitcoin.ECKey(seed).getBitcoinAddress().toString()
       
       el_address.textContent = address;
@@ -86,46 +93,49 @@ const bodyHTML = `<html>
   </body>
   </html>`
 
-/*
- * 3: Compile the app into a Data URL 
- */
-const sourceFile = `data:text/html;base64,` + btoa(bodyHTML)
+document.write(bodyHTML)
 
-/*
- * 4: Try to redirect to the Data URL
- * (Chrome and Firefox don't allow top-level navigation to Data URLs )
- */
-window.location = sourceFile
 
-/*
- * 5: Otherwise, fall back to a workaround
- * Ask user to install manually
- */
-document.write(`
-  <h2>Step 2: Bootloader Demo</h2>
-  <a href="${sourceFile}" class="installer">Drag me into your browser's tab bar</a>
-  <style>
-    .installer{
-      background-color: #00796b;
-      color:white;
-      fill:white;
-      font-family: system-ui;
-      display: flex;
-      align-items: center;
-      padding: 16px 24px;
-      border-radius: 64px;
-      box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
-            0 1px 8px 0 rgba(0, 0, 0, 0.12),
-            0 3px 3px -2px rgba(0, 0, 0, 0.4);
-        text-decoration: none;
-        font-size: 16px;
-        max-width: 360px;
-        margin-left: 8px;
-    }
+// /*
+//  * 3: Compile the app into a Data URL 
+//  */
+// const sourceFile = `data:text/html;base64,` + btoa(bodyHTML)
 
-    .installer:before {
-        content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 0 24 24' width='24'%3E%3Cpath fill='white' d='M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
-        margin-right: 16px;
-    }
-  </style>
-`)
+// /*
+//  * 4: Try to redirect to the Data URL
+//  * (Chrome and Firefox don't allow top-level navigation to Data URLs )
+//  */
+// window.location = sourceFile
+
+// /*
+//  * 5: Otherwise, fall back to a workaround
+//  * Ask user to install manually
+//  */
+// document.write(`
+//   <h2>Step 2: Bootloader Demo</h2>
+//   <a href="${sourceFile}" class="installer">Drag me into your browser's tab bar</a>
+//   <style>
+//     .installer{
+//       background-color: #00796b;
+//       color:white;
+//       fill:white;
+//       font-family: system-ui;
+//       display: flex;
+//       align-items: center;
+//       padding: 16px 24px;
+//       border-radius: 64px;
+//       box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
+//             0 1px 8px 0 rgba(0, 0, 0, 0.12),
+//             0 3px 3px -2px rgba(0, 0, 0, 0.4);
+//         text-decoration: none;
+//         font-size: 16px;
+//         max-width: 360px;
+//         margin-left: 8px;
+//     }
+
+//     .installer:before {
+//         content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 0 24 24' width='24'%3E%3Cpath fill='white' d='M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
+//         margin-right: 16px;
+//     }
+//   </style>
+// `)
